c07877cea368518de95daf1a920723f5795d5e08
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-05-17 11:48:08 +0200

    first commit

---
+++ e-shop-api/demo/src/test/java/com/eshop/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
