760c12a37866e2c02091df4fbd315b15b0c7d7ee
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-05-21 17:36:32 +0200

    created frontend components

---
+++ e-shop-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: "", pathMatch:"full", component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
