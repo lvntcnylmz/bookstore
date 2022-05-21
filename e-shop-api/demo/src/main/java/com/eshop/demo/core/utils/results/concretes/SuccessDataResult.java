e05d9b6670656625b920d128a82f49021c4c5a0b
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-05-21 15:40:35 +0200

    added result classes for responses

---
+++ e-shop-api/demo/src/main/java/com/eshop/demo/core/utils/results/concretes/SuccessDataResult.java
@@ -0,0 +1,13 @@
+package com.eshop.demo.core.utils.results.concretes;
+
+public class SuccessDataResult<T> extends DataResult<T> {
+
+    public SuccessDataResult(T data, String message) {
+        super(data, true, message);
+    }
+
+    public SuccessDataResult(T data) {
+        super(data, true);
+    }
+
+}
