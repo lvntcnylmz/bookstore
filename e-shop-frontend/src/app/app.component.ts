4b18fc17f6bee1b2a8bf126fcce6c1d7f2de90cc
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-05-21 18:53:14 +0200

    deploy new build

--- e-shop-frontend/src/app/app.component.ts
+++ e-shop-frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'e-shop-frontend';
+  title = 'E-Shop';
+
+  constructor(private router: Router) {}
+
+  hasRoute(route: string) {
+    return this.router.url.includes(route);
+  }
 }
