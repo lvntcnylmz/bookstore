5550118c1cd6fb3b608b7b0541683cca3d9357db
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-05-22 12:39:55 +0200

    added navi components

---
+++ e-shop-frontend/src/app/components/navi/navi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navi',
+  templateUrl: './navi.component.html',
+  styleUrls: ['./navi.component.sass']
+})
+export class NaviComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
