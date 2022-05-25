4ca4d8c58ed6e8b09fa78a2fbf29bff549e80aa9
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-05-25 16:19:39 +0200

    added controller exception

---
+++ e-shop-api/demo/src/main/java/com/eshop/demo/exceptions/GeneralExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.eshop.demo.exceptions;
+
+import com.eshop.demo.core.utils.results.concretes.ErrorDataResult;
+import com.eshop.demo.core.utils.results.concretes.ErrorResult;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+
+        Map<String, String> validationErrors = new HashMap<>();
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        ErrorDataResult<Object> errors = new ErrorDataResult<>(validationErrors, "Validation Errors...");
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<?> productNotFoundException(ProductNotFoundException exception){
+        return new ResponseEntity<>(new ErrorResult(exception.getLocalizedMessage()), HttpStatus.NOT_FOUND);
+    }
+
+}
