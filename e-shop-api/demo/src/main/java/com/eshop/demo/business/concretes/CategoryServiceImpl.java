f8e468e3fd88b1edf407f00261827c6364b6e2d7
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-06-01 14:18:51 +0200

    implemented new methods and new messages

--- e-shop-api/demo/src/main/java/com/eshop/demo/business/concretes/CategoryServiceImpl.java
+++ e-shop-api/demo/src/main/java/com/eshop/demo/business/concretes/CategoryServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 import com.eshop.demo.business.abstracts.CategoryService;
+import com.eshop.demo.core.utils.messages.Message;
 import com.eshop.demo.core.utils.results.abstracts.Result;
 import com.eshop.demo.core.utils.results.concretes.DataResult;
 import com.eshop.demo.core.utils.results.concretes.SuccessDataResult;
@@ -13,6 +14,7 @@ import com.eshop.demo.dtos.request.CategoryRequestDto;
 import com.eshop.demo.dtos.response.CategoryResponseDto;
 import com.eshop.demo.dtos.response.converter.CategoryDtoConverter;
 import com.eshop.demo.entities.concretes.Category;
+import com.eshop.demo.exceptions.CategoryNotFoundException;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -29,26 +31,34 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public Result add(CategoryRequestDto categoryRequest) {
         this.categoryRepository.save(this.convertToEntity(categoryRequest));
-        return new SuccessResult("The category added.");
+        return new SuccessResult(Message.SAVED);
     }
 
     @Override
     public DataResult<List<CategoryResponseDto>> getAll() {
-        return new SuccessDataResult<>(this.convertToDtoList(), "Categories are listed.");
+        return new SuccessDataResult<>(this.convertToDtoList(), Message.LISTED);
     }
 
     @Override
     public DataResult<List<CategoryResponseDto>> getAllSorted() {
-        return new SuccessDataResult<>(this.convertToDtoSortedList(), "Categories are listed.");
+        return new SuccessDataResult<>(this.convertToDtoSortedList(), Message.LISTED);
+    }
+
+    @Override
+    public DataResult<CategoryResponseDto> getById(Long id) {
+        Category category = this.categoryRepository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException(Message.NOT_FOUND_BY_ID));
+
+        return new SuccessDataResult<CategoryResponseDto>(this.convertToDto(category), Message.FOUND_BY_ID);
     }
 
     private Category convertToEntity(CategoryRequestDto categoryRequest) {
-        return new Category(categoryRequest.getCategoryName(), Collections.emptyList()); 
+        return new Category(categoryRequest.getCategoryName(), Collections.emptyList());
     }
 
-    // private CategoryResponseDto convertToDto(Category category) {
-    //     return CategoryDtoConverter.entityToDto(category);
-    // }
+    private CategoryResponseDto convertToDto(Category category) {
+        return CategoryDtoConverter.entityToDto(category);
+    }
 
     private List<CategoryResponseDto> convertToDtoList() {
         return CategoryDtoConverter.entityToDtoList(this.categoryRepository.findAll());
