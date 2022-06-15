6312ba4ba7bb1b23f424d2062d1d09b1c330934a
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-06-15 14:45:10 +0200

    changed test case

--- e-shop-frontend/src/app/app.component.spec.ts
+++ e-shop-frontend/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('e-shop-frontend app is running!');
+    expect(compiled.querySelector('title')?.textContent).toContain('e-shop-frontend');
   });
 });
