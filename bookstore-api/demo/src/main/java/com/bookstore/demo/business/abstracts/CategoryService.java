6d5a419f454a8866ab7b0c0f05f61bc4bcc864a6
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-06-20 13:59:32 +0200

    refactor api and services of category

--- bookstore-api/demo/src/main/java/com/bookstore/demo/business/abstracts/CategoryService.java
+++ bookstore-api/demo/src/main/java/com/bookstore/demo/business/abstracts/CategoryService.java
@@ -4,17 +4,9 @@ import java.util.List;
 
 import com.bookstore.demo.core.utils.results.abstracts.Result;
 import com.bookstore.demo.core.utils.results.concretes.DataResult;
-import com.bookstore.demo.dtos.request.CategoryRequestDto;
-import com.bookstore.demo.dtos.response.CategoryResponseDto;
 
 public interface CategoryService {
 
-    Result add(CategoryRequestDto categoryDto);
-
-    DataResult<List<CategoryResponseDto>> getAll();
-
-    DataResult<List<CategoryResponseDto>> getAllSorted();
-
-    DataResult<CategoryResponseDto> getById(Long id);
+    
 
 }
